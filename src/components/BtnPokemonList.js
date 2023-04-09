db4a56e50ef280ac8bd3b16cd6dd2eb5f59a8123
Luiz Casimiro <luizcasimi@gmail.com>
2023-04-09 13:54:50 -0300

    add blank lines + fix names

--- src/components/BtnPokemonList.js
+++ src/components/BtnPokemonList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router";
 
-function ButtonList() {
+function BtnPokemonList() {
   const navigate = useNavigate();
 
   return (
@@ -13,4 +13,4 @@ function ButtonList() {
     </div>
   )
 }
-export default ButtonList;
+export default BtnPokemonList;
